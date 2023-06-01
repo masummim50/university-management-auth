2f26a15d01c194dc9e2e3288f3a27f935bf751b0
unknown <masummim50@gmail.com>
2023-06-01 08:39:32 +0600

    checking husky

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
-
-import express, {Application, Request, Response} from "express"
-import cors from "cors"
-export const app:Application = express()
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+export const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-
-app.get('/', (req:Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('App Home Page')
 })
-
-
-
